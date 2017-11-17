f02c0df76faae892f49c511c1efaa15bf8285364
Haofu <haofu.zhu@gmail.com>
2017-11-17 17:29:56 +1100

    improve js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,43 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = [{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+}, {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+}, {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-hearts.png"
+}];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+
+const checkForMatch = () => {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+}
+
+const flipCard = cards => {
+  var cardID = 0;
+  console.log(cards[cardID].rank);
+
+}
+
+flipCard(cards);
